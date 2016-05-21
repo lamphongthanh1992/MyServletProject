46f25305b8d2d5e67bf8ff5ef17f7648b2026f37
lamphongthanh1992 <lamphongthanh1992@gmail.com>
2016-05-21 16:11:21 +0700

    asdasd
    ad

---
+++ src/Hello.java
@@ -0,0 +1,54 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    /**
+     * Default constructor. 
+     */
+    public Hello() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletConfig config=getServletConfig();  
+	    String name =config.getInitParameter("parametername"); 
+	    
+	    ServletContext context=getServletContext(); 
+	    String contextvalue =context.getInitParameter("contextkey");
+	    
+	    context.setAttribute("ex", "val");
+	    
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		out.print("<h1>Hello</h1>");
+		out.print("<h1>" + "Servlet Config: " +name + "</h1>");
+		out.print("<h1>" + "Servlet Context: " +contextvalue + "</h1>");
+		out.print("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
